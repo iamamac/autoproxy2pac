dac696df5832f88b00eaa2fe89be064918349f0c
Meng Xiangliang <911mxl@gmail.com>
2010-01-12 17:53:20 +0800

    Add RSS title, author, etc.

--- changelog.py
+++ changelog.py
@@ -27,13 +27,14 @@ class ChangelogJsonHandler(webapp.RequestHandler):
             self.error(412)
             return
         
-        changes = memcache.get('changelog/%s' % name)
-        if changes is None or len(changes) < fetchNum:
+        logs = memcache.get('changelog/%s' % name)
+        if logs is None or len(logs) < fetchNum:
             logs = ChangeLog.gql("WHERE ruleList = :1 ORDER BY date DESC", rules).fetch(fetchNum)
-            changes = [{'time'   : (l.date + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M"),
-                        'add'    : l.add,
-                        'remove' : l.remove} for l in logs]
-            memcache.add('changelog/%s' % name, changes)
+            memcache.add('changelog/%s' % name, logs)
+        
+        changes = [{'time'   : (l.date + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M"),
+                    'add'    : l.add,
+                    'remove' : l.remove} for l in logs]
         
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(json.dumps(changes[start:fetchNum]))
@@ -61,28 +62,26 @@ class ChangelogRssHandler(webapp.RequestHandler):
             self.error(412)
             return
         
-        changes = memcache.get('changelog/%s' % name)
-        if changes is None or len(changes) < fetchNum:
+        logs = memcache.get('changelog/%s' % name)
+        if logs is None or len(logs) < fetchNum:
             logs = ChangeLog.gql("WHERE ruleList = :1 ORDER BY date DESC", rules).fetch(fetchNum)
-            changes = [{'time'   : (l.date + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M"),
-                        'add'    : l.add,
-                        'remove' : l.remove} for l in logs]
-            memcache.add('changelog/%s' % name, changes)
+            memcache.add('changelog/%s' % name, logs)
         
         self.response.headers['Content-Type'] = 'application/rss+xml'
         
         path = os.path.join(os.path.dirname(__file__), 'changelogRssItem.html')
-        rss = RSS2(title="%s 近期更新内容" % name,
+        rss = RSS2(title="%s 更新记录" % name,
                    link=self.request.relative_url(name),
                    description="beta",
                    language="zh",
                    lastBuildDate=rules.date,
                    
-                   items=(RSSItem(title="",
-                                  description=template.render(path, i),
-                                  pubDate=i['time']) for i in changes))
+                   items=(RSSItem(title="%d月%d日 %s 更新: 增加 %d 条, 删除 %d 条" % (i.date.month, i.date.day, name, len(i.add), len(i.remove)),
+                                  author="gfwlist",
+                                  description=template.render(path, {'add':i.add, 'remove':i.remove}),
+                                  pubDate=i.date.strftime("%a, %d %b %Y %H:%M:%S +0800")) for i in logs))
         
-        rss.write_xml(self.response.out, "utf8")
+        rss.write_xml(self.response.out, "utf-8")
 
 class ChangelogHtmlHandler(webapp.RequestHandler):
     def get(self, name):
