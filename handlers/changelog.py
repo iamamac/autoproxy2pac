a95a596a47b114e106d2eff519cbf372e399c256
Meng Xiangliang <911mxl@gmail.com>
2010-04-08 14:49:35 +0800

    Little adjust to the earlier fix

--- handlers/changelog.py
+++ handlers/changelog.py
@@ -9,10 +9,11 @@ from util import template, webcached
 
 def getSampleUrlFromRule(rule):
     from urllib import unquote
+    rule = unquote(rule.encode())
     try:
-        rule = unquote(rule.encode()).decode('utf-8')
-    except:
-        rule = unquote(rule.encode()).decode('gbk', 'ignore')
+        rule = rule.decode('utf-8', 'strict')
+    except UnicodeDecodeError:
+        rule = rule.decode('gbk', 'ignore')
     if rule.startswith('||'): return 'http://' + rule[2:]
     if rule.startswith('.'): return 'http://' + rule[1:]
     if rule.startswith('|'): return rule[1:]
